d21edd83c72018962b157693f84e5d5bdb12b3f4
Annie Weng <annieweng@datamachines.io>
2018-12-17 13:13:24 -0500

    add bear token support

--- providers/oidc.go
+++ providers/oidc.go
@@ -65,7 +65,7 @@ func (p *OIDCProvider) Redeem(redirectURL, code string) (s *SessionState, err er
 
 	s = &SessionState{
 		AccessToken:  token.AccessToken,
-		IdToken:      jsonResponse.IdToken,
+		IdToken:      token.IdToken,
 		RefreshToken: token.RefreshToken,
 		ExpiresOn:    token.Expiry,
 		Email:        claims.Email,
